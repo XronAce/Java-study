8ac341e88b9be6d3c193ff1d111647553d742199
DonggiYang <xronace@gmail.com>
2022-08-02 00:01:57 +0900

    Complete

---
+++ java-study/baekjoon/Q1244.java
@@ -0,0 +1,54 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q1244 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numSwitch = sc.nextInt();
+		int[] switchStatus = new int[numSwitch];
+		for (int i=0; i<numSwitch; i++) {
+			switchStatus[i] = sc.nextInt();
+		}
+		
+		int numStudent = sc.nextInt();
+		
+		for(int studentNo=1; studentNo<=numStudent; studentNo++) {
+			int gender = sc.nextInt();
+			int switchNo = sc.nextInt();
+			
+			if (gender == 1) { // 남자일 경우
+				for (int i=switchNo-1; i<numSwitch; i+=switchNo) {
+					if (switchStatus[i] == 1) switchStatus[i] = 0;
+					else switchStatus[i] = 1;
+				}
+			} else {
+				int inc = 0;
+				boolean flag = true;
+				while(switchNo-1-inc>=0 && switchNo-1+inc<numSwitch && flag) {
+					if (switchStatus[switchNo-1-inc] == switchStatus[switchNo-1+inc]) {
+						if (switchStatus[switchNo-1-inc] == 1) {
+							switchStatus[switchNo-1-inc] = 0;
+							switchStatus[switchNo-1+inc] = 0;
+						} else {
+							switchStatus[switchNo-1-inc] = 1;
+							switchStatus[switchNo-1+inc] = 1;
+						}
+					} else {
+						flag = false;
+					}
+					inc++;
+				}
+			}
+		}
+		for (int i=0; i<switchStatus.length; i++) {
+			System.out.print(switchStatus[i]+" ");
+			if((i+1)%20 == 0)
+				System.out.println();
+		}
+		
+		
+		sc.close();
+	}
+}
