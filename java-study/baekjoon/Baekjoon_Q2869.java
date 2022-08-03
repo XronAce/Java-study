f24e3f0fdab69ed5cbcc7053952ad02e7f13b6a9
DonggiYang <xronace@gmail.com>
2022-08-04 02:30:45 +0900

    Complete

--- java-study/baekjoon/Baekjoon_Q2869.java
+++ java-study/baekjoon/Baekjoon_Q2869.java
@@ -1,5 +1,22 @@
 package baekjoon;
 
-public class Baekjoon_Q2869 {
+import java.util.Scanner;
 
+public class Baekjoon_Q2869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+
+        int dayCount = (V - B) / (A - B);
+
+        if ((V - B) % (A - B) != 0) {
+            dayCount++;
+        }
+
+        System.out.println(dayCount);
+        sc.close();
+    }
 }
