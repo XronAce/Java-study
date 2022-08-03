49e652ec3177ae8129d56f47ae216faef94269bc
DonggiYang <xronace@gmail.com>
2022-08-04 02:23:13 +0900

    fixed file name

---
+++ java-study/baekjoon/Baekjoon_Q2798.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_Q2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] cards = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int temp = cards[i] + cards[j] + cards[k];
+
+                    if (temp <= M) {
+                        answer = Math.max(answer, temp);
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
