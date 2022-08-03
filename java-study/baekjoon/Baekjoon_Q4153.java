834d05e01c8745688a04fb98a8ced6711a38db1e
DonggiYang <xronace@gmail.com>
2022-08-04 01:32:32 +0900

    Complete

--- java-study/baekjoon/Baekjoon_Q4153.java
+++ java-study/baekjoon/Baekjoon_Q4153.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Baekjoon_Q4153 {
@@ -12,12 +13,14 @@ public class Baekjoon_Q4153 {
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
+            int[] sides = new int[3];
+            for (int i = 0; i < 3; i++) {
+                sides[i] = Integer.parseInt(st.nextToken());
+            }
 
-            if (A != 0) {
-                if (C * C == A * A + B * B)
+            Arrays.sort(sides);
+            if (sides[0] != 0) {
+                if (sides[2] * sides[2] == sides[0] * sides[0] + sides[1] * sides[1])
                     System.out.println("right");
                 else
                     System.out.println("wrong");
