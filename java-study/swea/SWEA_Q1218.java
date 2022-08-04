f38299d37d5154d6333777fe6c4b68fe30d20818
DonggiYang <xronace@gmail.com>
2022-08-04 13:57:39 +0900

    Complete - need to check vulnerability

---
+++ java-study/swea/SWEA_Q1218.java
@@ -0,0 +1,55 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA_Q1218 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int test_case = 1; test_case < 11; test_case++) {
+            int length = Integer.parseInt(br.readLine());
+            String[] brackets = br.readLine().split("");
+            Boolean isCorrect = true;
+
+            int numBracOpenS = 0; // 열려있는 ()괄호의 개수
+            int numBracOpenM = 0; // 열려있는 {}괄호의 개수
+            int numBracOpenB = 0; // 열려있는 []괄호의 개수
+            int numBracOpenP = 0; // 열려있는 <>괄호의 개수
+
+            for (int i = 0; i < length; i++) {
+                if (brackets[i].equals("("))
+                    numBracOpenS++;
+                else if (brackets[i].equals("{"))
+                    numBracOpenM++;
+                else if (brackets[i].equals("["))
+                    numBracOpenB++;
+                else if (brackets[i].equals("<"))
+                    numBracOpenP++;
+                else if (brackets[i].equals(")"))
+                    numBracOpenS--;
+                else if (brackets[i].equals("}"))
+                    numBracOpenM--;
+                else if (brackets[i].equals("]"))
+                    numBracOpenB--;
+                else if (brackets[i].equals(">"))
+                    numBracOpenP--;
+
+            }
+
+            if (numBracOpenS != 0 || numBracOpenM != 0 || numBracOpenB != 0 || numBracOpenP != 0) {
+                isCorrect = false;
+            } else {
+                isCorrect = true;
+            }
+
+            if (isCorrect) {
+                System.out.println("#" + test_case + " " + "1");
+            } else {
+                System.out.println("#" + test_case + " " + "0");
+            }
+
+        }
+    }
+}
