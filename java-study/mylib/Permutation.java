79add9779599691318a2c3329cbae84423b7c6df
DonggiYang <xronace@gmail.com>
2022-08-04 17:48:00 +0900

    blueprint

---
+++ java-study/mylib/Permutation.java
@@ -0,0 +1,14 @@
+package mylib;
+
+import java.util.List;
+
+public class Permutation {
+    static <T> List<T> permutation() {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
