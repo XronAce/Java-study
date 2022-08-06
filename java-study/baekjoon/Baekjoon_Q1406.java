86728088ec0b42bbd35044f1931fd1e9e32adc2e
DonggiYang <xronace@gmail.com>
2022-08-07 02:06:27 +0900

    Complete - used stack to pass time limit

--- java-study/baekjoon/Baekjoon_Q1406.java
+++ java-study/baekjoon/Baekjoon_Q1406.java
@@ -1,51 +1,52 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Baekjoon_Q1406 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String s = br.readLine();
 
-        List<Character> editor = new ArrayList<>();
+        Stack<Character> leftside = new Stack<>();
+        Stack<Character> rightside = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            editor.add(s.charAt(i));
+            leftside.push(s.charAt(i));
         }
 
         int M = Integer.parseInt(br.readLine());
-        int cursorPositionIdx = editor.size(); // 커서의 위치는 editor.get(cursorPositionIdx-1)의 뒤에 있다. 0일 경우 맨 앞에 위치하고 있다.
 
         for (int command_case = 1; command_case <= M; command_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             String command = st.nextToken();
 
-            if (command.equals("L") && cursorPositionIdx != 0) {
-                cursorPositionIdx--;
-            } else if (command.equals("D") && cursorPositionIdx != editor.size()) {
-                cursorPositionIdx++;
-            } else if (command.equals("B") && cursorPositionIdx != 0) {
-                cursorPositionIdx--;
-                editor.remove(cursorPositionIdx);
+            if (command.equals("L") && !leftside.empty()) {
+                rightside.push(leftside.pop());
+            } else if (command.equals("D") && !rightside.empty()) {
+                leftside.push(rightside.pop());
+            } else if (command.equals("B") && !leftside.empty()) {
+                leftside.pop();
             } else if (command.equals("P")) {
-                editor.add(cursorPositionIdx, st.nextToken().charAt(0));
-                cursorPositionIdx++;
+                leftside.push(st.nextToken().charAt(0));
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < editor.size(); i++) {
-            sb.append(editor.get(i));
+        while (!leftside.empty()) {
+            rightside.push(leftside.pop());
         }
 
-        s = sb.toString();
+        while (!rightside.empty()) {
+            bw.write(rightside.pop());
+        }
 
-        System.out.println(s);
-
+        bw.flush();
+        bw.close();
     }
 }
