8193350a87e1c05653917e21cdc79607fa85158d
DonggiYang <xronace@gmail.com>
2022-08-07 17:42:16 +0900

    Complete - using comparator

--- java-study/baekjoon/Baekjoon_Q11650.java
+++ java-study/baekjoon/Baekjoon_Q11650.java
@@ -3,13 +3,9 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
-import java.util.Map.Entry;
 
 public class Baekjoon_Q11650 {
     public static void main(String[] args) throws IOException {
@@ -18,36 +14,28 @@ public class Baekjoon_Q11650 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
-
-        int[] x = new int[N];
-        int[] y = new int[N];
+        int[][] coordinate = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            x[i] = Integer.parseInt(st.nextToken());
-            y[i] = Integer.parseInt(st.nextToken());
+            coordinate[i][0] = Integer.parseInt(st.nextToken());
+            coordinate[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Map<Integer, List<Integer>> coordinate = new HashMap<>();
+        Arrays.sort(coordinate, new Comparator<int[]>() {
 
-        for (int i = 0; i < N; i++) {
-            List<Integer> temp = new ArrayList<>();
-            if (!coordinate.containsKey(x[i]))
-                coordinate.put(x[i], temp);
-        }
-
-        for (int i = 0; i < N; i++) {
-            coordinate.get(x[i]).add(y[i]);
-        }
-
-        for (int i = 0; i < N; i++) {
-            Collections.sort(coordinate.get(x[i]));
-        }
-
-        for (Entry<Integer, List<Integer>> e : coordinate.entrySet()) {
-            for (int i = 0; i < e.getValue().size(); i++) {
-                System.out.println(e.getKey() + " " + e.getValue().get(i));
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
             }
+        });
+
+        for (int[] innerArr : coordinate) {
+            System.out.println(innerArr[0] + " " + innerArr[1]);
         }
 
     }
