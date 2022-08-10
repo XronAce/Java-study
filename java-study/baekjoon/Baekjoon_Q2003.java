ed1379f93eb4f1b874bf1a628bb3329df1eede58
DonggiYang <luxdefuror@gmail.com>
2022-08-10 14:47:49 +0900

    Complete

--- java-study/baekjoon/Baekjoon_Q2003.java
+++ java-study/baekjoon/Baekjoon_Q2003.java
@@ -6,30 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Baekjoon_Q2003 {
-    static int Cnt = 0;
-
-    public static void combination(int[] arr, boolean[] visited, int start, int depth, int r) {
-        if (depth == r) {
-            // Change code to get required output at here.
-            int[] a = new int[r];
-            int idx = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if (visited[i])
-                    a[idx++] = arr[i];
-            }
-            // if ()
-            System.out.println();
-
-            return;
-        }
-        for (int i = start; i < arr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                combination(arr, visited, i + 1, depth + 1, r);
-                visited[i] = false;
-            }
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -39,13 +15,31 @@ class Baekjoon_Q2003 {
         int M = Integer.parseInt(st.nextToken());
 
         int[] num = new int[N];
-        int[] idxarr = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             num[i] = Integer.parseInt(st.nextToken());
-            idxarr[i] = i;
         }
 
-        System.out.println(Cnt);
+        int cnt = 0;
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum = num[i];
+            if (sum == M) {
+                cnt++;
+                continue;
+            }
+            for (int j = i + 1; j < N; j++) {
+                sum += num[j];
+                if (sum == M) {
+                    cnt++;
+                    break;
+                } else if (sum > M) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+
     }
 }
