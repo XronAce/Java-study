b5ebd1b9bdff467654e5e4bf9e7ec7e66bfa6bd4
DonggiYang <xronace@gmail.com>
2022-08-16 01:16:11 +0900

    Complete - Combination with dfs

--- java-study/baekjoon/Baekjoon_Q6603.java
+++ java-study/baekjoon/Baekjoon_Q6603.java
@@ -6,12 +6,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon_Q6603 {
+    static StringBuilder sb;
+
+    public static void combination(int[] arr, boolean[] isSelected, int cnt, int start) {
+        if (cnt == 6) {
+            for (int i = 0; i < arr.length; i++) {
+                if (isSelected[i]) {
+                    sb.append(arr[i]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        for (int i = start; i < arr.length; i++) {
+            isSelected[i] = true;
+            combination(arr, isSelected, cnt + 1, i + 1);
+            isSelected[i] = false;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st;
         while (true) {
+            sb = new StringBuilder();
             st = new StringTokenizer(br.readLine(), " ");
             int N = Integer.parseInt(st.nextToken());
             if (N == 0) {
@@ -23,6 +41,9 @@ public class Baekjoon_Q6603 {
                 S[i] = Integer.parseInt(st.nextToken());
             }
 
+            combination(S, new boolean[N], 0, 0);
+
+            System.out.println(sb);
         }
 
     }
