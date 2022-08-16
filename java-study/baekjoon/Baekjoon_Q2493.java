2c83d1067f64475327edcf88cdf6901f7e936473
DonggiYang <xronace@gmail.com>
2022-08-16 13:39:47 +0900

    Complete - Stack

--- java-study/baekjoon/Baekjoon_Q2493.java
+++ java-study/baekjoon/Baekjoon_Q2493.java
@@ -7,53 +7,35 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Baekjoon_Q2493 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        int[] towers = new int[N];
-        Stack<Integer> answer = new Stack<>();
-        Stack<Integer> tower = new Stack<>();
+        Stack<int[]> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            int t = Integer.parseInt(st.nextToken());
-            tower.push(t);
-            towers[i] = t;
-        }
 
-        int idx = N - 1;
-        boolean flag = false;
-        while (!tower.empty()) {
-            int i = tower.pop();
-            idx--;
-            if (tower.empty()) {
-                answer.push(0);
-                break;
-            }
-            if (tower.peek() < i) {
-                for (int index = idx; index >= 0; index--) {
-                    if (towers[index] >= i) {
-                        answer.push(index);
-                        flag = true;
+        for (int i = 0; i < N; i++) {
+            int height = Integer.parseInt(st.nextToken());
+
+            while (!stack.empty()) {
+                if (!stack.empty()) {
+                    if (stack.peek()[0] < height) {
+                        stack.pop();
+                    } else {
+                        sb.append(stack.peek()[1]).append(" ");
                         break;
                     }
                 }
-                if (flag == false) {
-                    answer.push(0);
-                }
-            } else {
-                answer.push(idx);
             }
+
+            if (stack.empty()) {
+                sb.append(0).append(" ");
+            }
+            stack.push(new int[] { height, i + 1 });
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        while (!answer.empty()) {
-            sb.append(answer.pop());
-            sb.append(" ");
-        }
-        String answer1 = sb.toString();
-        System.out.println(answer1);
+        System.out.println(sb);
     }
 }
