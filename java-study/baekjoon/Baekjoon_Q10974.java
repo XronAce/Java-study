dce768710091de9c6d78650e0f3715f3407c9d86
DonggiYang <luxdefuror@gmail.com>
2022-08-17 14:07:22 +0900

    Complete - permutation

--- java-study/baekjoon/Baekjoon_Q10974.java
+++ java-study/baekjoon/Baekjoon_Q10974.java
@@ -3,18 +3,21 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Baekjoon_Q10974 {
-    static void permutation(int[] arr, int[] out, boolean[] visited, int depth, int r) {
-        if (depth == r) {
-            for (int e : out) {
-                System.out.print(e + " ");
-            }
-            return;
-        }
+    static StringBuilder sb = new StringBuilder();
 
+    public static void permutation(int[] arr, int[] out, boolean[] isSelected, int cnt, int r) {
+        if (cnt == r) {
+            for (int e : out) {
+                sb.append(e + " ");
+            }
+            sb.append("\n");
+        }
         for (int i = 0; i < arr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                // out[depth] =
+            if (!isSelected[i]) {
+                isSelected[i] = true;
+                out[cnt] = arr[i];
+                permutation(arr, out, isSelected, cnt + 1, r);
+                isSelected[i] = false;
             }
         }
     }
@@ -25,11 +28,17 @@ public class Baekjoon_Q10974 {
         int N = sc.nextInt();
         int[] arr = new int[N];
         for (int i = 1; i < N + 1; i++) {
-            arr[i] = i;
+            arr[i - 1] = i;
         }
-
         permutation(arr, new int[N], new boolean[N], 0, N);
 
+        System.out.print(sb);
         sc.close();
     }
 }
+
+/*
+ * Permutation.
+ * using stringbuilder decreased runtime from 2380ms to 280ms.
+ * passing r rather than arr.length fixed timeout issue.
+ */
