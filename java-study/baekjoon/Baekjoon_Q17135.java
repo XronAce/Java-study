0739e281ef9bfd29980f4f65cc0d388a33b0a9db
DonggiYang <luxdefuror@gmail.com>
2022-08-23 14:26:30 +0900

    update

--- java-study/baekjoon/Baekjoon_Q17135.java
+++ java-study/baekjoon/Baekjoon_Q17135.java
@@ -1,6 +1,38 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Baekjoon_Q17135 {
+    public static int N, M, D;
+    public static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+
+        map = new int[N + 1][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // int stage = 0;
+        // while (stage != N) {
+        // stage++; // stage 시작
+        // attack();
+        // nextRound();
+        // }
+
+    }
 
 }
 
