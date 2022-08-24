356a789aa4abea4961e061128c1274a159ab476c
DonggiYang <luxdefuror@gmail.com>
2022-08-24 16:32:54 +0900

    Complete - using bfs with only one queue haha

--- java-study/baekjoon/Baekjoon_Q3055.java
+++ java-study/baekjoon/Baekjoon_Q3055.java
@@ -7,25 +7,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Coordinate {
-    int r, c;
-
-    public Coordinate(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-}
-
-public class Baekjoon_Q3055 {
-    public static int R, C, ans;
+class Baekjoon_Q3055 {
+    public static int timeAns, R, C, hedgeR, hedgeC, destR, destC;
     public static char[][] map;
-    public static int startR, startC, destR, destC;
+    public static int[][] timeMap;
     public static int[] dx = { -1, 1, 0, 0 };
     public static int[] dy = { 0, 0, -1, 1 };
     public static boolean[][] isVisited;
-    public static int[][] intMap;
-    public static Queue<Coordinate> move;
-    public static boolean isDone;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,59 +22,62 @@ public class Baekjoon_Q3055 {
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-
         map = new char[R][C];
-        intMap = new int[R][C];
+        timeMap = new int[R][C];
         isVisited = new boolean[R][C];
-        move = new LinkedList<>();
+
         for (int i = 0; i < R; i++) {
             s = br.readLine();
             for (int j = 0; j < C; j++) {
                 map[i][j] = s.charAt(j);
                 if (map[i][j] == 'S') {
-                    startR = i;
-                    startC = j;
+                    hedgeR = i;
+                    hedgeC = j;
                 } else if (map[i][j] == 'D') {
                     destR = i;
                     destC = j;
                 }
             }
         }
-        Coordinate start = new Coordinate(startR, startC);
 
-        floodFill();
-        bfs(start);
+        timeAns = 0;
+        bfs(hedgeR, hedgeC);
 
-        while (!move.isEmpty()) {
-            floodFill();
-            bfs(move.poll());
-        }
-
-        if (intMap[destR][destC] != 0) {
-            System.out.println(intMap[destR][destC]);
-        } else {
-            System.out.println("KAKTUS");
-        }
-
+        System.out.println("KAKTUS");
     }
 
-    public static void bfs(Coordinate start) {
-        isVisited[start.r][start.c] = true;
-        for (int i = 0; i < 4; i++) {
-            int newR = start.r + dx[i];
-            int newC = start.c + dy[i];
+    public static void bfs(int hedgeR, int hedgeC) {
+        Queue<int[]> que = new LinkedList<>();
+        int[] temp = { hedgeR, hedgeC };
+        que.offer(temp);
+        int queSize = 0;
 
-            if (newR >= 0 && newC >= 0 && newR < R && newC < C) {
-                if (map[newR][newC] != '*' && map[newR][newC] != 'X' && !isVisited[newR][newC]) {
-                    Coordinate temp = new Coordinate(newR, newC);
-                    move.offer(temp);
-                    isVisited[newR][newC] = true;
-                    intMap[newR][newC] = intMap[start.r][start.c] + 1;
-                    map[newR][newC] = '@';
+        while (!que.isEmpty()) {
+            queSize = que.size();
+            timeAns++;
+            floodFill();
+            for (int time = 0; time < queSize; time++) {
+                int[] cur = que.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int newR = cur[0] + dx[i];
+                    int newC = cur[1] + dy[i];
+
+                    if (newR >= 0 && newC >= 0 && newR < R && newC < C) {
+                        if (!isVisited[newR][newC] && map[newR][newC] != '*' && map[newR][newC] != 'X') {
+                            isVisited[newR][newC] = true;
+                            int[] temp1 = { newR, newC };
+                            que.offer(temp1);
+                            timeMap[newR][newC] = timeMap[cur[0]][cur[1]] + 1;
+                        }
+                    }
                 }
             }
+            if (timeMap[destR][destC] != 0) {
+                System.out.println(timeAns);
+                System.exit(0);
+            }
         }
-
     }
 
     public static void floodFill() {
@@ -96,6 +87,7 @@ public class Baekjoon_Q3055 {
                     for (int k = 0; k < 4; k++) {
                         int nR = i + dx[k];
                         int nC = j + dy[k];
+
                         if (nR >= 0 && nC >= 0 && nR < R && nC < C) {
                             if (map[nR][nC] == '.') {
                                 map[nR][nC] = '&';
@@ -113,6 +105,5 @@ public class Baekjoon_Q3055 {
                 }
             }
         }
-
     }
 }
