572ba8d24a8f7bad2d42f2ed76e5aff527040077
DonggiYang <xronace@gmail.com>
2022-10-04 17:53:25 +0900

    renamed file

--- java-study/baekjoon/Baekjoon_Q1463.java
+++ java-study/baekjoon/Baekjoon_Q1463.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon_1463 {
+public class Baekjoon_Q1463 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
