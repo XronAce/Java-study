0b63458b3059f6ee652b779fdbc1535f8d4f2844
DonggiYang <xronace@gmail.com>
2022-10-07 15:50:02 +0900

    Complete - using bfs, adjmatrix

--- java-study/swea/SWEA_Q5643.java
+++ java-study/swea/SWEA_Q5643.java
@@ -3,14 +3,13 @@ package swea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class SWEA_Q5643 {
-    public static List<Integer> smaller = new ArrayList<>();
-    public static List<Integer> taller = new ArrayList<>();
     public static int N, M;
+    public static int tallerCnt, smallerCnt;
     public static int[][] comparison;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -22,66 +21,65 @@ public class SWEA_Q5643 {
         for (int test_case = 1; test_case <= T; test_case++) {
             N = Integer.parseInt(br.readLine());
             M = Integer.parseInt(br.readLine());
-            comparison = new int[M][2];
+            comparison = new int[N+1][N+1];
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine(), " ");
-                comparison[i][0] = Integer.parseInt(st.nextToken());
-                comparison[i][1] = Integer.parseInt(st.nextToken());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                comparison[a][b] = 1;
             }
-            // 입력 처리 완료
-            int checkCnt = 0;
+
             int ans = 0;
 
-            // smallerCheck(1);
-            // tallerCheck(1);
-            // System.out.println(smaller.toString());
-            // System.out.println(taller.toString());
-            
             for (int i = 1; i <= N; i++) {
-                smallerCheck(i);
+                tallerCnt = 0;
+                smallerCnt = 0;
                 tallerCheck(i);
-                checkCnt += smaller.size() + taller.size();
-                if (checkCnt == N - 1) {
+                smallerCheck(i);
+                if (smallerCnt + tallerCnt == N - 1) {
                     ans++;
                 }
-                smaller.clear();
-                taller.clear();
-                checkCnt = 0;
             }
 
             System.out.println("#"+test_case+" "+ans);
         }
     }
-    
-    public static void smallerCheck(int thanWho) {
-        int cnt = 0;
-        for (int i = 0; i < M; i++) {
-            if (comparison[i][1] == thanWho) {
-                cnt++;
-                if (!smaller.contains(comparison[i][0])) {
-                    smaller.add(comparison[i][0]);
+
+    private static void tallerCheck(int thanWho) {
+        boolean[] isVisited = new boolean[N + 1];
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(thanWho);
+        isVisited[thanWho] = true;
+        
+        while (!que.isEmpty()) {
+            int temp = que.poll();
+            for (int j = 1; j <= N; j++) {
+                if (!isVisited[j] && comparison[temp][j] == 1) {
+                    isVisited[j] = true;
+                    que.offer(j);
+                    tallerCnt++;
                 }
-                smallerCheck(comparison[i][0]);
             }
-        }
-        if (cnt == 0) {
-            return;
         }
     }
 
-    public static void tallerCheck(int thanWho) {
-        int cnt = 0;
-        for (int i = 0; i < M; i++) {
-            if (comparison[i][0] == thanWho) {
-                cnt++;
-                if (!taller.contains(comparison[i][1])) {
-                    taller.add(comparison[i][1]);
+    private static void smallerCheck(int thanWho) {
+        boolean[] isVisited = new boolean[N + 1];
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(thanWho);
+        isVisited[thanWho] = true;
+
+        while (!que.isEmpty()) {
+            int temp = que.poll();
+            for (int i = 1; i <= N; i++) {
+                if (!isVisited[i] && comparison[i][temp] == 1) {
+                    isVisited[i] = true;
+                    que.offer(i);
+                    smallerCnt++;
                 }
-                tallerCheck(comparison[i][1]);
             }
         }
-        if (cnt == 0) {
-            return;
-        }
     }
+    
+
 }
